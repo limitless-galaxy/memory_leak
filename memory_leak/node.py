1278cbbd6f5a6b6ea105f7d8b717c13b92066dc6
Tzumx <z.m.dev@outlook.com>
2022-11-29 21:06:39 +0200

    remove unnecessary

--- memory_leak/node.py
+++ memory_leak/node.py
@@ -23,9 +23,6 @@ from nautilus_trader.core.datetime import dt_to_unix_nanos
 from nautilus_trader.model.data.tick import QuoteTick
 from nautilus_trader.model.identifiers import InstrumentId, Venue
 
-DATEDIFF = 946684800000000000
-
-
 def get_perp_market():
     """
     Get info about instrument for parsing
@@ -94,15 +91,7 @@ class BatchesBacktestNode(BacktestNode):
             for account in engine.cache.accounts():
                 for balance in account.last_event.balances:
                     engine._log.info(str(balance), LogColor.GREEN)
-            engine.cache.flush_db()
-            while len(data["data"]) > 0:
-                item = data["data"].pop()
-                del item
-            del data
-            while len(data_quote) > 0:
-                item = data_quote.pop()
-                del item
-            del data_quote
+            # engine.cache.flush_db()
             # data_quote.clear()
             # data.clear()
             engine.clear_data()
@@ -112,33 +101,27 @@ class BatchesBacktestNode(BacktestNode):
         print(engine.trader.generate_account_report(Venue("BINANCE")))
         print("Fee report:")
         print(engine.trader.generate_order_fills_report())
-        # print(engine.trader.generate_positions_report())
         print("Order report:")
         print(engine.trader.generate_orders_report())
         engine.end_streaming()
         engine.dispose()
 
     def get_data(self, data_configs: list[BacktestDataConfig], timestep) -> dict:
+
         start_times = {config.instrument_id: config.start_time for config in data_configs}
         end_times = {config.instrument_id: config.end_time for config in data_configs}
         start_time = min(start_times.values())
         current_time = start_time
         end_time = max(end_times.values())
+
         aggregate_list_quoteticks = []
+        fake_data = self.get_fake_ticks(date_start=current_time, date_stop=current_time+timestep)
+        aggregate_list_quoteticks.extend(self.parse_fake_data_to_quotetick(fake_data, data_configs[0].instrument_id))
+
         while current_time < end_time:
             till_time = current_time + timestep
             if till_time > end_time:
                 till_time = end_time
-            while len(aggregate_list_quoteticks) > 0:
-                item = aggregate_list_quoteticks.pop()
-                del item
-            # aggregate_list_quoteticks.clear()
-            aggregate_list_quoteticks = []
-            for config in data_configs:
-                instrument_id = config.instrument_id.split("-")[0]
-                fake_data = self.get_fake_ticks(date_start=current_time, date_stop=till_time)
-                aggregate_list_quoteticks.extend(self.parse_fake_data_to_quotetick(fake_data, config.instrument_id, 0))
-                del fake_data
             yield aggregate_list_quoteticks
             current_time = till_time
 
@@ -154,13 +137,11 @@ class BatchesBacktestNode(BacktestNode):
         print(f"Number of entries: {len(data_list)}")
         return data_list
 
-    def parse_fake_data_to_quotetick(self, data: list, instrument_id_value: str, drop_ratio: int = 0) -> QuoteTick:
+    def parse_fake_data_to_quotetick(self, data: list, instrument_id_value: str) -> QuoteTick:
         for row in data:
             time = row[0]
-            if random.randint(0, 99) < drop_ratio:
-                continue
-            ask, bid = self.check_precisions(str(row[1]), str(row[2]))
-            askvol, bidvol = self.check_precisions(str(row[3]), str(row[4]))
+            ask, bid = str(row[1]), str(row[2])
+            askvol, bidvol = str(row[3]), str(row[4])
             data_qt = {
                 "instrument_id": instrument_id_value,
                 "bid": bid,
@@ -174,16 +155,4 @@ class BatchesBacktestNode(BacktestNode):
             data_qt.clear()
             del row
 
-    def check_precisions(self, number1: str, number2: str):
-        """Set the same precisions to both numbers and back it as string"""
-        if "." in number1:
-            len1 = len(number1.split(".")[1])
-        else:
-            len1 = 0
-        if "." in number2:
-            len2 = len(number2.split(".")[1])
-        else:
-            len2 = 0
-        precision = max(len1, len2)
-        zeros = f".{precision}f"
-        return format(float(number1), zeros), format(float(number2), zeros)
+
